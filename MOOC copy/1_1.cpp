1af6ae6636515fc16dad82d30373ccfe0907d0ec
Chentao-Xu <ct.xu@outlook.com>
2022-10-08 09:14:22 +0800

    增加几个大项目

---
+++ MOOC copy/1_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (int a = 2; a <= n;a++)
+    {
+        for (int b = 1; b <= a ;b++)
+        {
+            for (int c = b; c <= a;c++)
+            {
+                for (int d = c; d <= a;d++)
+                {
+                    if (a*a*a == b*b*b + c*c*c + d*d*d)
+                        cout << "Cube = " << a << ", Triple = (" << b << "," << c << "," << d << ')' << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
+//这是一条注释
